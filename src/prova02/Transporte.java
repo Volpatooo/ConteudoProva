4b9492ad72f234f46bfe66f93ec632fe165c5f82
Mateus Volpato Cirico <teteuvolpato@gmail.com>
2025-11-18 17:25:07 -0300

    Conteudo Prova 03 completo

---
+++ src/prova02/Transporte.java
@@ -0,0 +1,8 @@
+package prova02;
+
+public interface Transporte {
+
+    public double calcularTarifa(double distancia);
+
+	int calcularTempoEstimado(double distancia);
+} 
---
+++ src/prova02/TransporteBicicleta.java
@@ -0,0 +1,18 @@
+package prova02;
+
+public class TransporteBicicleta extends ServicoTransporte{
+    	@Override
+	public double getTempoMedio() {
+		return 3;
+	}
+
+	@Override
+	public double getPrecoTarifa() {
+		return 0.80;
+	}
+
+	@Override
+	public String getDescricao() {
+		return "Locação de bicicletas compartilhadas";
+	}
+}
